a6bb9804924b26945a210dc0d384f99c7dd296f3
Olena Pyslar <pislar33@gmail.com>
2026-06-30 23:12:39 +0300

    Solution Fixed2

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ import requests
 
 def get_weather() -> None:
     api_key = os.environ.get("API_KEY")
-    BASE_URL = "http://api.weatherapi.com/v1/current.json"
-    CITY = "Paris"
-    response = requests.get(BASE_URL, params={"key": api_key, "q": CITY})
+    base_url = "http://api.weatherapi.com/v1/current.json"
+    city = "Paris"
+    response = requests.get(base_url, params={"key": api_key, "q": city})
     data = response.json()
     temp = data["current"]["temp_c"]
     condition = data["current"]["condition"]["text"]
-    print(f"Weather in {CITY}: {temp}°C, {condition}")
+    print(f"Weather in {city}: {temp}°C, {condition}")
 
 
 if __name__ == "__main__":
