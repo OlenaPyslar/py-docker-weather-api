27f3922bfdc3050828b8d8c63aa5f0d7ebb15f4b
Olena Pyslar <pislar33@gmail.com>
2026-07-02 23:18:29 +0300

    Solution Fixed

--- app/main.py
+++ app/main.py
@@ -7,15 +7,19 @@ def get_weather() -> None:
     if not api_key:
         print("API_KEY is missing")
         return
+
     base_url = "http://api.weatherapi.com/v1/current.json"
     city = "Paris"
+
     response = requests.get(base_url, params={"key": api_key, "q": city})
     if response.status_code != 200:
         print(f"Error: {response.status_code}")
         return
+
     data = response.json()
-    temp = data["current"]["temp_c"]
-    condition = data["current"]["condition"]["text"]
+    current_data = data.get("current", {})
+    temp = current_data.get("temp_c")
+    condition = current_data.get("condition", {}).get("text")
     print(f"Weather in {city}: {temp}°C, {condition}")
 
 
